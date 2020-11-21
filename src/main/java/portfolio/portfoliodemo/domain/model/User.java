28a2d4b29c9f495c50fc2409fcacd481df3fdbae
fallen_a99 <fallen_a99@o2.pl>
2020-11-21 13:05:52 +0100

    structure :: update initial package structure

---
+++ src/main/java/portfolio/portfoliodemo/domain/model/User.java
@@ -0,0 +1,28 @@
+package portfolio.portfoliodemo.domain.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder @EqualsAndHashCode(of = "username") @ToString
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String password;
+    private Boolean active = Boolean.FALSE;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "username", referencedColumnName = "username"),indexes =
+        @Index(name = "users_roles_username_idx", columnList = "user_name"))
+    @Column(name = "role")
+    private Set<String> roles;
+}
