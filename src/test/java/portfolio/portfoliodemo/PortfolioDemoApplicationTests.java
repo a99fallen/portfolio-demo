ddbff06e796624a8580e5b824ec7cd2edfec31fb
fallen_a99 <fallen_a99@o2.pl>
2020-11-21 10:26:37 +0100

    structure :: initial project structure

---
+++ src/test/java/portfolio/portfoliodemo/PortfolioDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.portfoliodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
