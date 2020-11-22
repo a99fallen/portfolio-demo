199e5c7b95e232913e37eb43396a16647ad7fde8
fallen_a99 <fallen_a99@o2.pl>
2020-11-22 09:32:18 +0100

    register-user :: custom user authentication

--- src/main/java/portfolio/portfoliodemo/domain/repository/UserRepository.java
+++ src/main/java/portfolio/portfoliodemo/domain/repository/UserRepository.java
@@ -3,6 +3,10 @@ package portfolio.portfoliodemo.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import portfolio.portfoliodemo.domain.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
