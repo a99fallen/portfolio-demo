38f500358c35b3af211dec7ebeb928d80ed4de67
fallen_a99 <fallen_a99@o2.pl>
2020-11-22 10:59:15 +0100

    register-user :: edit user command

--- src/main/java/portfolio/portfoliodemo/web/controller/UserProfileController.java
+++ src/main/java/portfolio/portfoliodemo/web/controller/UserProfileController.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import portfolio.portfoliodemo.data.user.UserSummary;
 import portfolio.portfoliodemo.service.UserService;
+import portfolio.portfoliodemo.web.command.EditUserCommand;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/profile")
@@ -19,7 +24,23 @@ public class UserProfileController {
     @GetMapping
     public String getProfilePage (Model model) {
         UserSummary summary = userService.getCurrentUserSummary();
+        EditUserCommand editUserCommand = createEditUserCommand(summary);
         model.addAttribute("userSummary", new UserSummary());
+        model.addAttribute("editUserCommand", editUserCommand);
         return "user/profile";
+    }
+
+    private EditUserCommand createEditUserCommand(UserSummary summary) {
+        return EditUserCommand.builder()
+                .firstName(summary.getFirstName())
+                .lastName(summary.getLastName())
+                .birthDate(summary.getBirthDate())
+                .build();
+    }
+
+    @PostMapping("/edit")
+    public String editUserProfile(@Valid EditUserCommand editUserCommand, BindingResult bindingResult) {
+        //TODO Do implementacji
+        return "rediret:/profile";
     }
 }
